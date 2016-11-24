cf4d5f72d1f7728c42df8ec06a4d359c8a1dd003
Marcus Hammer <marcus.hammer@virtual7.de>
2016-11-24 10:46:21 +0100

    Bugfix bei Verzeichniserstellung

--- 00_copy_script.sh
+++ 00_copy_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir /app/testdatenvorbereitung
+mkdir -p /app/testdatenvorbereitung
 cp -r * /app/testdatenvorbereitung/
 chown -R jenkins:jenkins /app/testdatenvorbereitung
 chmod +x /app/testdatenvorbereitung/*.sh
