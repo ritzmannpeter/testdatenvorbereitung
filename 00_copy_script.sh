8eafd2e0f0f5ac0c414084f6b638e076a7f376f2
Marcus Hammer <marcus.hammer@virtual7.de>
2016-11-24 12:43:30 +0100

    fix Berechtigungen

--- 00_copy_script.sh
+++ 00_copy_script.sh
@@ -4,4 +4,4 @@ ssh oracle@10.1.116.20 'rm -rf /app/testdatenvorbereitung; mkdir -p /app/testdat
 
 scp -r * oracle@10.1.116.20:/app/testdatenvorbereitung/
 
-ssh oracle@10.1.116.20 'chown -R jenkins:jenkins /app/testdatenvorbereitung; chmod +x /app/testdatenvorbereitung/*.sh;'
+ssh oracle@10.1.116.20 'chown -R oracle:oinstall /app/testdatenvorbereitung; chmod +x /app/testdatenvorbereitung/*.sh;'
