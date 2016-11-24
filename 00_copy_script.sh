8f6c3adcf1b43219a0997103a2cafa48ed4641d0
Marcus Hammer <marcus.hammer@virtual7.de>
2016-11-24 12:41:13 +0100

    Aufräumen mit integriert

--- 00_copy_script.sh
+++ 00_copy_script.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-#ssh oracle@10.1.116.20
-#rm -rf /app/testdatenvorbereitung
-#mkdir -p /app/testdatenvorbereitung
-#exit
+ssh oracle@10.1.116.20 'rm -rf /app/testdatenvorbereitung; mkdir -p /app/testdatenvorbereitung;'
 
 scp -r * oracle@10.1.116.20:/app/testdatenvorbereitung/
 
-
-#ssh oracle@10.1.116.20
-#chown -R jenkins:jenkins /app/testdatenvorbereitung
-#chmod +x /app/testdatenvorbereitung/*.sh
-#exit
+ssh oracle@10.1.116.20 'chown -R jenkins:jenkins /app/testdatenvorbereitung; chmod +x /app/testdatenvorbereitung/*.sh;'
