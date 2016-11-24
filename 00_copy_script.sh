0d796e12b1f778725b495000b3bade1efa720a37
Marcus Hammer <marcus.hammer@virtual7.de>
2016-11-24 11:39:21 +0100

    Fix: Kopieren auf andere Maschine

--- 00_copy_script.sh
+++ 00_copy_script.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+ssh oracle@10.1.116.20
 rm -rf /app/testdatenvorbereitung
 mkdir -p /app/testdatenvorbereitung
-cp -r * /app/testdatenvorbereitung/
+exit
+
+scp -R * oracle@10.1.116.20:/app/testdatenvorbereitung/
+
+
+ssh oracle@10.1.116.20
 chown -R jenkins:jenkins /app/testdatenvorbereitung
 chmod +x /app/testdatenvorbereitung/*.sh
+exit
