3ec71bcb6ebcedc407e9e3dc5ca9a6e71495d7b2
pritzmann <pritzmann@kzvbw.de>
2017-01-26 16:57:45 +0100

    Variable für instant client Nutzung in stammdatenimportieren.sh exportiert Tabellenlisten an aktuelle Version angepasst Konfiguration eingefügt

--- stammdatenimportieren.sh
+++ stammdatenimportieren.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 #
 cd /app/testdatenvorbereitung
-
-export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
-
+# Verzeichnis, in dem der Oracle Instant Client abgelegt ist
+INSTANT_CLIENT="/home/oracle/instantclient_12_1"
+# Workaround wegen ungewollter Umlaute-Konvertierung
+export NLS_LANG=AMERICAN_AMERICA.WE8MSWIN1252
+# Umgebungsvariablen
+export PATH="${INSTANT_CLIENT}:${PATH}"
+export TNS_ADMIN="${INSTANT_CLIENT}"
+export LD_LIBRARY_PATH="${INSTANT_CLIENT}"
+if [ "$1" = "Entwicklung" ]; then
+   export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+fi
+if [ "$1" = "TST" ]; then
+   export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+fi
+if [ "$1" = "TST1" ]; then
+   export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+fi
+if [ "$1" = "TST2" ]; then
+   export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+fi
 let RC=0
 msg="keine Importbeschreibung fuer $1 vorhanden"
 while IFS=, read identifier connection targetschema sourceschema sourcepassword sourcedatabase;do
@@ -11,7 +28,6 @@ while IFS=, read identifier connection targetschema sourceschema sourcepassword 
      ./linkerstellen.sh $connection sourcelink $sourceschema $sourcepassword $sourcedatabase
      ./linktesten.sh $connection sourcelink
      let RC=$?
-     echo "RC = $RC"
      if [ $RC !=  0 ]; then
        echo "Abbruch aufgrund von Problemen beim erstellen des Datenbank Link, Fehlercode: $RC"
        cat sqlplus.out 
@@ -29,3 +45,5 @@ while IFS=, read identifier connection targetschema sourceschema sourcepassword 
    fi
 done < config.txt
 echo $msg
+
+
